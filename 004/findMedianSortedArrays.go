92742ff5d70f8fbdf73cd9fc41d738d8f4488a74
Antoni Guss <antoniguss0@gmail.com>
2024-12-28 19:46:25 +0100

    Initial commit, 004 solved

---
+++ 004/findMedianSortedArrays.go
@@ -0,0 +1,36 @@
+package main
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	length := len(nums1) + len(nums2)
+	nums := make([]int, length)
+	// Merge two arrays
+	i, j, k := 0, 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] <= nums2[j] {
+			nums[k] = nums1[i]
+			i++
+		} else {
+			nums[k] = nums2[j]
+			j++
+		}
+		k++
+	}
+
+	for i < len(nums1) {
+		nums[k] = nums1[i]
+		k++
+		i++
+	}
+
+	for j < len(nums2) {
+		nums[k] = nums2[j]
+		k++
+		j++
+	}
+
+	if length%2 == 1 {
+		return float64(nums[length/2])
+	}
+
+	return float64((nums[length/2] + nums[length/2+1]) / 2.0)
+}
